36fba832809e272a49b094fe19de2206a77b99c0
Zach <zhegwood@gmail.com>
2026-08-31 22:33:36 -0600

    moved geolocation and timezone to pinia stores

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
-const timezone = useState("timezone");
+import { storeToRefs } from "pinia";
+import { useTimezoneStore } from "~/stores/timezone";
+import { useGeolocationStore } from "~/stores/geolocation";
+
+const { timezone } = storeToRefs(useTimezoneStore());
+const { geolocation } = storeToRefs(useGeolocationStore());
 </script>
 <template>
   <h1 class="h1">Forecast</h1>
   {{ timezone }}
+  <pre>{{ geolocation }}</pre>
   <Forecast />
 </template>
